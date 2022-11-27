d5ba48d43ad3de52c85f6607f9f12a98d9f40b55
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2022-11-27 14:33:43 +0300

    playSound method added: - fixed found nil while unwrapping an optional value error which was when the button was pressed. - plain button type to default button type fixed this error.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation //Audio Visual Foundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+   
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        // DispatchQueue: Uygulamanızın ana iş parçacığında veya bir arka plan iş parçacığında görevlerin seri veya eşzamanlı olarak yürütülmesini yöneten bir nesne.
+        
+        // asyncAfter: Belirtilen öznitelikleri kullanarak yürütme için bir blok zamanlar ve hemen geri döner.
+        // asenkron --> zaman uyumsuz çalışma gerçekleştirilir --> delay
+    }
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!) //try! try catch kısaltması
+        player?.play()
+        
+    }
+    
+    
 
+
+    
 
 }
-
